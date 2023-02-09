6d311efd508295a48992d4b0e4ecf879f5cd80ff
Nils <nils.dachwig@gmail.com>
2023-02-09 16:45:05 +0100

    Generate random order

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import sys
 import argparse
+import random
 
 
 class QuestionGenerator:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, file):
+        self.filename = file
 
     def get_questions(self):
         with open(self.filename) as f:
             questions = [line.rstrip() for line in f]
         return(questions)
-    
+
+    def create_random_order(self):
+        question_list = self.get_questions()
+
+        random_list = []
+        while len(question_list) != 0:
+            question_count = len(question_list)
+            random_index = random.randint(0, question_count-1)
+            random_list.append(question_list[random_index])
+            del question_list[random_index]
+
+        return(random_list)
+
     def run(self):
-        self.get_questions()
+        question_list = self.create_random_order()
+        print("Finished!")
+        print(question_list)
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
