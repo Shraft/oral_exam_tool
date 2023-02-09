0da8d23051e5a8ae71cad6e8efe5bead3af19834
Nils <nils.dachwig@gmail.com>
2023-02-09 16:53:41 +0100

    Code improvement
    random.shuffle(list) mischt liste selbstständig

--- main.py
+++ main.py
@@ -10,24 +10,15 @@ class QuestionGenerator:
     def get_questions(self):
         with open(self.filename) as f:
             questions = [line.rstrip() for line in f]
+        random.shuffle(questions)
         return(questions)
 
-    def create_random_order(self):
-        question_list = self.get_questions()
-
-        random_list = []
-        while len(question_list) != 0:
-            question_count = len(question_list)
-            random_index = random.randint(0, question_count-1)
-            random_list.append(question_list[random_index])
-            del question_list[random_index]
-
-        return(random_list)
-
     def run(self):
-        question_list = self.create_random_order()
+        question_list = self.get_questions()
         print("Finished!")
         print(question_list)
+        
+
         
 
 if __name__ == "__main__":
