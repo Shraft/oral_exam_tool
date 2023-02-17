8c52a4eb3e984240ed4fce1e55836aae2756429c
Jannek Steinke <jannek.steinke@mailbox.tu-dresden.de>
2023-02-17 12:48:56 +0100

    added clear in terminal, added script and folie splitted

--- oral_exam_tool.py
+++ oral_exam_tool.py
@@ -17,8 +17,15 @@ class DisplayGenerator:
 
     def show_question(self, question, num_questions):
         question_string, script = self.decrypt_question(question)
-        
-        print(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n########### Script: \033[0;34;49m{script}\033[0m ########### Fragen übrig: [\033[0;33;49m{num_questions}\033[0m] ###########\n#")
+        os.system("clear")
+        if script == "unknown":
+            script_number = "-"
+            folie_number = "-"
+        else:
+            script = script.replace("S", "")
+            script_number = script.split("F")[0]
+            folie_number = script.split("F")[1]
+        print(f"########### Script:\033[0;34;49m{script_number}]\033[0m, Folie: \033[0;34;49m[{folie_number}\033[0m ########## Fragen übrig: [\033[0;33;49m{num_questions}\033[0m] ###########\n#")
         print(f"#\t{question_string}")
 
     def print_red(self, text, optional=""):
