a0a3c1263ceffbb0777ca7a23a43c19fd4a22d0e
Jannek Steinke <jannek.steinke@mailbox.tu-dresden.de>
2023-02-17 13:26:51 +0100

    added indent

--- oral_exam_tool.py
+++ oral_exam_tool.py
@@ -28,7 +28,7 @@ class DisplayGenerator:
             script = script.replace("]", "")
             script_number = script.split("F")[0]
             folie_number = script.split("F")[1]
-        print(f"########### Script:\033[0;34;49m{script_number}\033[0m, Folie: \033[0;34;49m{folie_number}\033[0m ########## Fragen übrig: [\033[0;33;49m{num_questions}\033[0m] ###########\n#")
+        print(f"########### Script: \033[0;34;49m{script_number}\033[0m, Folie: \033[0;34;49m{folie_number}\033[0m ########## Fragen übrig: [\033[0;33;49m{num_questions}\033[0m] ###########\n#")
         print(f"#\t{question_string}\n#")
 
     def print_red(self, text, optional=""):
